9a5155790a9ad044652283b3548451e52427cfbe
Aluno <aluno@ifpe>
2023-10-11 09:01:55 -0300

    teste

--- src/rotas.jsx
+++ src/rotas.jsx
@@ -4,6 +4,7 @@ import CadastroProduto from "./views/produto/CadastroProduto";
 import  Login  from "./views/Login/index";
 import Home from "./views/home/home";
 import Pedido from "./views/pedido/pedido";
+import CadEmpresa from "./views/CadEmpresa/index"
 
 function Rotas(){
     return (
@@ -13,6 +14,7 @@ function Rotas(){
         <Route path="/cad" element={<CadastroProduto/>}/>
         <Route path="/home" element={<Home/>}/>
         <Route path="/pedido" element={<Pedido/>}/>
+        <Route path="/cad-empresa" element={<CadEmpresa/>}/>
 
 
     </Routes>
